b69abd6dae8ceeeaa18e12a7e0f0ea48dccfba4e
Ian12-sgv <ianbonillo@gmial.com>
2025-01-06 00:55:01 -0400

    listo

--- src/db.js
+++ src/db.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost/Inicio");
+    await mongoose.connect(
+      "mongodb+srv://iqanillo2:z6FHWpMWuJnEH7lT@cluster1.ras4i.mongodb.net/Inicio"
+    );
     console.log("Base de datos conectada");
   } catch (error) {
     console.error(error);
